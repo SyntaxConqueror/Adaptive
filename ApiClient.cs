142f92d95becbc0fd9b27d85573d3d778482dcfa
Artem Shvets <47186073+SyntaxConqueror@users.noreply.github.com>
2024-02-17 16:16:03 +0200

    first commit

---
+++ ApiClient.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace LR5
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public dynamic Data { get; set; }
+    }
+
+    public class ApiClient
+    {
+       
+        private HttpClient _httpClient;
+
+        public ApiClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+
+        public async Task<ApiResponse<T>> Get<T>(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    
+                    JToken parsedJson = JToken.Parse(jsonString);
+                
+                    JArray resultsArray = (JArray)parsedJson["results"];
+
+                    return new ApiResponse<T> { StatusCode = response.StatusCode, Data = resultsArray };
+                }
+                else
+                {
+                    return new ApiResponse<T> { StatusCode = response.StatusCode };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<T> { StatusCode = HttpStatusCode.InternalServerError, Message = ex.Message };
+            }
+        }
+
+        public async Task<ApiResponse<T>> Post<T>(string url, HttpContent content)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.PostAsync(url, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    
+                    return new ApiResponse<T> { StatusCode = response.StatusCode, Data = jsonString };
+                }
+                else
+                {
+                    return new ApiResponse<T> { StatusCode = response.StatusCode };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<T> { StatusCode = HttpStatusCode.InternalServerError, Message = ex.Message };
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using LR5;
+using Microsoft.Extensions.Logging;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var apiClient = new ApiClient();
+
+        var getResponse = await apiClient.Get<string>("https://randomuser.me/api/");
+        Console.WriteLine($"GET Response: Status Code - {getResponse.StatusCode}, Message - {getResponse.Message}, Data - {getResponse.Data}");
+
+        var postContent = new StringContent("Helloworld");
+        var postResponse = await apiClient.Post<int>("https://www.toptal.com/developers/postbin/1708178012162-3688768388237", postContent);
+        Console.WriteLine($"POST Response: Status Code - {postResponse.StatusCode}, Message - {postResponse.Message}, Data - {postResponse.Data}");
+    }
+}
