351aed44678ab2329265c0425843b923cfee17a0
Artem Shvets <47186073+SyntaxConqueror@users.noreply.github.com>
2024-02-28 11:32:16 +0200

    fix: Fix service registration, add comments

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IPostService, PostService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<IUserService, UserService>(); //  ќдин екземпл€р серв≥су буде створений дл€ кожного HTTP-запиту
+                                                         //  ÷е означаЇ, що вс≥ запити, €к≥ оброблюютьс€ в рамках одного HTTP-запиту, будуть використовувати той самий екземпл€р серв≥су
+
+builder.Services.AddSingleton<IPostService, PostService>(); // ќдин екземпл€р серв≥су буде створений на всю тривал≥сть роботи додатку
+                                                            // ÷е означаЇ, що кожний HTTP-запит буде використовувати той самий екземпл€р серв≥су
+
+builder.Services.AddTransient<ICommentService, CommentService>(); // ƒл€ кожного виклику серв≥су буде створений новий екземпл€р
+                                                                  // ÷е означаЇ, що кожен раз, коли викликаЇтьс€ серв≥с, буде отримано новий екземпл€р
 
 var app = builder.Build();
 
--- obj/Debug/net7.0/LR7.AssemblyInfo.cs
+++ obj/Debug/net7.0/LR7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LR7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3edca36a9ad573162757b47948b0a54410f7689f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LR7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LR7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
